81929efb751347a6b9b7c84d02fd4d41cb22c423
ВашÐPischalnikovEMÐ <lusiferik96@gmail.com>
2025-04-15 20:02:45 +0300

    Add First Code

---
+++ src/test/java/PischalnikovEM2.java
@@ -0,0 +1,18 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+    static {
+        Configuration.holdBrowserOpen = true; // Браузер останется открыт
+    }
+
+    @Test
+    void successfulSearchTest() {
+        open("https://duckduckgo.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=r1-0]").shouldHave(text("https://selenide.org"));
+    }
+}
